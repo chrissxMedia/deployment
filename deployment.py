507eebf8189b0d91a5f8c705782190031970bc08
pixel <chrissx@chrissx.de>
2021-09-01 06:40:27 +0200

    initial version
    i hacked it up in like 20 minutes and deployed it, it works.

---
+++ deployment.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from csv import reader
+from os import chdir
+from os.path import exists
+from subprocess import run
+from time import sleep
+
+def shell(cmd, **kwargs):
+    run(cmd, shell=True, check=True, **kwargs)
+
+shell('npm install -g siwedt')
+
+deployments = []
+with open('/etc/deployments.csv') as f:
+    for line in reader(f):
+        deployments.append(tuple(line))
+        if not exists(line[0]):
+            shell(f'git clone "{line[1]}" "{line[0]}"')
+
+while True:
+    for deployment in deployments:
+        try:
+            chdir(deployment[0])
+            shell('git pull')
+            shell(deployment[2])
+        except:
+            pass
+        sleep(30)
