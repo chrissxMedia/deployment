f34f00bc884a195fdf3757f2b721bbaf65c0f206
pixel <pixel@chrissx.de>
2023-04-28 07:52:40 +0200

    be more verbose

--- deployment.py
+++ deployment.py
@@ -4,13 +4,15 @@ from os import chdir
 from os.path import exists
 from subprocess import run
 from time import sleep
+from traceback import print_exc
 
 
 def shell(cmd, **kwargs):
+    print(cmd)
     run(cmd, shell=True, check=True, **kwargs)
 
 deployments = []
-with open('/etc/deployments.csv') as f:
+with open('/etc/deployments.csv', encoding='utf-8') as f:
     for line in reader(f):
         deployments.append(tuple(line))
         if not exists(line[0]):
@@ -19,13 +21,14 @@ with open('/etc/deployments.csv') as f:
 while True:
     for deployment in deployments:
         try:
-            print('cd: ' + deployment[0])
+            print('cd ' + deployment[0])
             chdir(deployment[0])
             shell('git pull')
             if not exists('deploy'):
                 shell(deployment[2])
             else:
+                # TODO: redirected stdout/err and check=False
                 run('deploy')
         except Exception as e:
-            print('Exception: ' + str(e))
+            print_exc()
         sleep(30)
